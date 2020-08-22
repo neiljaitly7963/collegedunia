0456b6ab0d0f613f2bf5613c813d4bae5fcbe84b
neiljaitly7963 <neiljaitly7963@gmail.com>
2020-08-22 13:42:31 +0530

    Updates

---
+++ precache-manifest.be5cd0914257f130b31571b4c2d3f3b8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4975dc117f3f6e43a4a4e0e3b5b2b7e6",
+    "url": "/collegedunia/index.html"
+  },
+  {
+    "revision": "7ef47827a76949d83c6c",
+    "url": "/collegedunia/static/css/main.13801c35.chunk.css"
+  },
+  {
+    "revision": "9f8cc9f8f6cd3cb88c6c",
+    "url": "/collegedunia/static/js/2.6a962539.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/collegedunia/static/js/2.6a962539.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ef47827a76949d83c6c",
+    "url": "/collegedunia/static/js/main.18f8ec50.chunk.js"
+  },
+  {
+    "revision": "c877f58d6d38b82a28f5",
+    "url": "/collegedunia/static/js/runtime-main.b8d60d25.js"
+  },
+  {
+    "revision": "619ee97eae1c5e93f5d68a9157b2466c",
+    "url": "/collegedunia/static/media/college_02.619ee97e.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/collegedunia/static/media/logo.5d5d9eef.svg"
+  }
+]);
